ebd221bb1dc20711a024a81309eaf7fad4ef27c3
doerrzoran <doerrzoan@gmail.com>
2025-09-27 12:05:45 +0200

    api role

--- backend/src/Entity/Role.php
+++ backend/src/Entity/Role.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\RoleRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: RoleRepository::class)]
+#[ApiResource]
 class Role
 {
     public const ROLE_EMPLOYE = 'ROLE_EMPLOYE';
